208b996b2bf2beed39e8857f5cb1e85a4c1265ab
Yasmin <yasminrodrigues.barros@ucsal.edu.br>
2025-08-28 15:14:17 -0300

    Final Test - Build Sucessful

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
 import br.com.mariojp.solid.ocp.*;
 
 public class Main {
 	public static void main(String[] args) {
-		DiscountCalculator calc = new DiscountCalculator();
+		Map<CustomerType,DiscountPolicy> policies = new HashMap<>(); //inicia 
+		policies.put(CustomerType.REGULAR, new RegularPolicy());
+        policies.put(CustomerType.PREMIUM, new PremiumPolicy());
+        policies.put(CustomerType.PARTNER, new PartnerPolicy());
+
+		DiscountCalculator calc = new DiscountCalculator(policies);
 		System.out.println("REGULAR 100 -> " + calc.apply(100, CustomerType.REGULAR));
 		System.out.println("PREMIUM 100 -> " + calc.apply(100, CustomerType.PREMIUM));
 		// PARTNER deveria ter 12%
--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -1,11 +1,22 @@
 package br.com.mariojp.solid.ocp;
 
+import java.util.Map;
+
 public class DiscountCalculator {
-    public double apply(double amount, CustomerType type){
-        switch (type){
-            case REGULAR: return amount * 0.95;
-            case PREMIUM: return amount * 0.90;
-            default: return amount;
-        }
+    private Map<CustomerType, DiscountPolicy> policies;
+  
+      public DiscountCalculator(Map<CustomerType, DiscountPolicy> policies) {
+        this.policies = policies;
     }
+
+    public double apply(double amount, CustomerType type) {
+        DiscountPolicy policy = policies.get(type); //pega dentro do mapa a política correspondente ao tipo de cliente
+        if (policy == null) {
+            return amount; //padrão default
+        }
+        return policy.apply(amount);
+    
+    }
+
+    
 }
